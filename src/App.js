2cc22bd306432c61507e38e851a0eff030e9ee28
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2019-12-05 13:02:50 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <div>
+        <div>
+
+
+        <div class="gcse-search"></div>
+        </div>
+      </div>
+      <div>
+        <a href="C:\Users\Owner\webdev\react-hooks-no-redux">
+          C:\Users\Owner\webdev\react-hooks-no-redux
+        </a>
+        <p>
+          <a href="C:\Users\Owner\local-home\notes.txt">NOTES</a>
+        </p>
+        <p>
+          <a href="C:\Users\Owner\local-home">DEV</a>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
